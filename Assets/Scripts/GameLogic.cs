fe69f7d74059a04106d8a9be97ab35374ab2bd59
Melina Morales <melina.morales@mi.unc.edu.ar>
2026-04-17 20:21:23 -0300

    scene not optimized

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject _buttonStart;
+    [SerializeField] private PlayableDirector _director;
+    [SerializeField] private GameObject _timeline;
+
+    public void StartCinematic()
+    {
+        _buttonStart.SetActive(false);
+        Debug.Log(Application.persistentDataPath);
+        FPSDisplay.Instance.StartClock();
+        _director.Play();
+    }
+
+    private void OnEnable()
+    {
+        _director.stopped += OnTimelineStopped;
+    }
+
+    private void OnDisable()
+    {
+        _director.stopped -= OnTimelineStopped;
+    }
+    private void OnTimelineStopped(PlayableDirector director)
+    {
+        string fpsListString = "[" + string.Join(",", FPSDisplay.Instance.fpsList) + "]";
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/FPS_" + DateTime.Now.ToString("dd.MM.yyyy_HH-mm-ss") + ".txt", fpsListString);
+
+        FPSDisplay.Instance.StopClock();
+        _buttonStart.SetActive(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 3DTestEnvironment
+# 3D Test Environment
+
+Project created to analyze and optimize a [Unity Scene](https://assetstore.unity.com/packages/3d/environments/stylized-house-interior-224331).
